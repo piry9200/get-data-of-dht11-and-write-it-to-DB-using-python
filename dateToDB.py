115db59784ce45ae605f62b99e9665a382c00509
piry9200 <ymt104ro09@gmail.com>
2022-06-15 15:30:29 +0900

    来ちゃああああああ　データベースに温度などを登録するコードを追加

--- dateToDB.py
+++ dateToDB.py
@@ -20,19 +20,19 @@ cur.execute("CREATE TABLE IF NOT EXISTS data (id,date, temp, humi)")
 
 try:
 	while True:
-	    result = instance.read()
-	    if result.is_valid():
-	        print("Last valid input: " + str(datetime.datetime.now()))
-	        print("Temperature: %-3.1f C" % result.temperature)
-	        print("Humidity: %-3.1f %%" % result.humidity)
-            cur.execute(f"INSERT INTO data VALUES({id}, {str(datetime.datetime.now())}, {result.temperature}, {result.humidity})")
-            id = id + 1
+		result = instance.read()
+		if result.is_valid():
+			print("Last valid input: " + str(datetime.datetime.now()))
+			print("Temperature: %-3.1f C" % result.temperature)
+			print("Humidity: %-3.1f %%" % result.humidity)
+			cur.execute(f"INSERT INTO data VALUES (?, ?, ?, ?)", [id, str(datetime.datetime.now()), result.temperature, result.humidity])
+			id = id + 1
+			conn.commit()
 
-
-	    time.sleep(6)
+	time.sleep(6)
 
 except KeyboardInterrupt:
-    print("Cleanup")
-    GPIO.cleanup()
+	print("Cleanup")
+	GPIO.cleanup()
 
 conn.close()
