74cabd6c1ba785d68f77b68e52ebd8a5f868cc39
piry9200 <ymt104ro09@gmail.com>
2022-10-08 20:07:21 +0900

    json作成コード完成

--- getSomeDatas/createJson.py
+++ getSomeDatas/createJson.py
@@ -27,16 +27,21 @@ for i in range(0,numOfdate[0][0]):
 
 dictOftemp = {}
 for i in range(0, numOftemp[0][0]):
-	dictOftemp[i] = temps[i][0]
+	dictOftemp[i] = str(temps[i][0]) + '°C'
 
 dictOfhumi = {}
 for i in range(0, numOftemp[0][0]):
-	dictOfhumi[i] = humis[i][0]
+	dictOfhumi[i] = str(humis[i][0]) + '%'
 
-print(dates)
-print(temps)
-print(humis)
-print()
 print(dictOfdate)
 print(dictOftemp)
 print(dictOfhumi)
+
+with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfdate.json","w")as f:
+	json.dump(dictOfdate, f, ensure_ascii=False, indent=4)
+
+with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOftemp.json","w")as f:
+        json.dump(dictOftemp, f, ensure_ascii=False, indent=4)
+
+with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfhumi.json","w")as f:
+        json.dump(dictOfhumi, f, ensure_ascii=False, indent=4)  
