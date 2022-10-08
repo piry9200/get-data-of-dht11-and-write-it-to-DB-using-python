bef6924f1c1c3108b820c6bb0e6a7cd2ebf05bdf
piry9200 <ymt104ro09@gmail.com>
2022-10-08 19:01:00 +0900

    DBに入力する日時を時間と分だけにした＆json作成テスト

--- getSomeDatas/createJson.py
+++ getSomeDatas/createJson.py
@@ -6,16 +6,37 @@ dic = {}
 conn = sqlite3.connect(DATABASE)
 cur = conn.cursor()
 
-cur.execute("SELECT COUNT(date) FROM data")
+cur.execute("SELECT date FROM data")
 dates = cur.fetchall()
+cur.execute("SELECT COUNT(date) FROM data")
+numOfdate = cur.fetchall()
 
-cur.execute("SELECT COUNT(temp) FROM data")
+cur.execute("SELECT temp FROM data")
 temps = cur.fetchall()
+cur.execute("SELECT COUNT(temp) FROM data")
+numOftemp = cur.fetchall()
 
-cur.execute("SELECT COUNT(humi) FROM data")
+cur.execute("SELECT humi FROM data")
 humis = cur.fetchall()
+cur.execute("SELECT COUNT(humi) FROM data")
+numOfhumi = cur.fetchall()
+
+dictOfdate = {}
+for i in range(0,numOfdate[0][0]):
+	dictOfdate[i] = dates[i][0]
+
+dictOftemp = {}
+for i in range(0, numOftemp[0][0]):
+	dictOftemp[i] = temps[i][0]
+
+dictOfhumi = {}
+for i in range(0, numOftemp[0][0]):
+	dictOfhumi[i] = humis[i][0]
 
 print(dates)
 print(temps)
 print(humis)
-
+print()
+print(dictOfdate)
+print(dictOftemp)
+print(dictOfhumi)
