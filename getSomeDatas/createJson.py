bfe05377a2b95eff6d43dae04dc7df2760f8ddda
piry9200 <ymt104ro09@gmail.com>
2022-10-10 13:13:40 +0900

    日時温度湿度をひとつのJSONにまとめるようにした

--- getSomeDatas/createJson.py
+++ getSomeDatas/createJson.py
@@ -33,22 +33,14 @@ dictOfHumi = {}
 for i in range(0, numOftemp[0][0]):
 	dictOfHumi[i] = str(humis[i][0]) + '%'
 
-# dictOfAll={date : dictOfDate,         {[date、temperature、humidityをキーにもち]、[それぞれの要素に「idをキーに持ち,値を要素として持つ」オブジェクト]}
-#            temperature : dictOfTemp
-#            humidity : dictOfHumi
-#           }
-# with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfAll.json","w")as f:
-#	json.dump(dictOfAll, f, ensure_ascii=False, indent=4)
+dictOfAll={'date' : dictOfDate,        # {[date、temperature、humidityをキーにもち]、[それぞれの要素に「idをキーに持ち,値を要素として持つ」オブジェクト]}
+           'temperature' : dictOfTemp,
+           'humidity' : dictOfHumi
+          }
+with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfAlldata.json","w")as f:
+	json.dump(dictOfAll, f, ensure_ascii=False, indent=4)
 
-print(dictOfDate)
-print(dictOfTemp)
-print(dictOfHumi)
-
-with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfdate.json","w")as f:
-	json.dump(dictOfDate, f, ensure_ascii=False, indent=4)
-
-with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOftemp.json","w")as f:
-        json.dump(dictOfTemp, f, ensure_ascii=False, indent=4)
-
-with open("/home/pi/pri_prg/dht11_python_db/getSomeDatas/jsonOfhumi.json","w")as f:
-        json.dump(dictOfHumi, f, ensure_ascii=False, indent=4)  
+#print(dictOfDate)
+#print(dictOfTemp)
+#print(dictOfHumi)
+#print(dictOfAll)
